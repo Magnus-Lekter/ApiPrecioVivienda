0c23ba9eb7f8c4d3ebf295ebadd57ff4e9fa8c3d
Magnus-Lekter <man.vargasc@duocuc.cl>
2024-11-24 02:38:00 -0300

    same

--- run.py
+++ run.py
@@ -1,20 +1,15 @@
 import logging
-import sys
-import joblib
 from app import create_app
-from flask import Flask, request, jsonify
-import traceback
-import pandas as pd
-import numpy as np
-from flask_cors import CORS
+
+
 
 
 
 if __name__ == '__main__':
     app = create_app()      
-    model = joblib.load('modelo_vivienda.pkl') # Load "modelo_vivienda.pkl"
+    #model = joblib.load('modelo_vivienda.pkl') # Load "modelo_vivienda.pkl"
     logging.info('Modelo cargado')
-    model_columns = joblib.load('model_columns.pkl') # Load "model_columns.pkl"
+    #model_columns = joblib.load('model_columns.pkl') # Load "model_columns.pkl"
     logging.info('Columnas cargadas')
     #app.run(port=port, debug=True)
     app.run()
