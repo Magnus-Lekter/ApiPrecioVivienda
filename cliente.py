e16bf9c3e398e978e0bd235316e13f3e81763092
Magnus-Lekter <man.vargasc@duocuc.cl>
2024-11-25 00:59:38 -0300

    cambios en api modelo.py, cliente y archivo .csv

--- cliente.py
+++ cliente.py
@@ -1,6 +1,6 @@
 import requests
 import matplotlib.pyplot as plt
-url = 'http://127.0.0.1:12345/predict'
+url = 'http://127.0.0.1:5000/predict'
 #data = [{
 #    "habitaciones": 3,
 #    "pobreza": 10,
@@ -8,48 +8,52 @@ url = 'http://127.0.0.1:12345/predict'
 #    "año": 2024
 #}]
 headers = {'Content-Type': 'application/json'}
+vivienda = {
+            "area_total": 139,
+            "habitaciones": 6,
+            "tipo_vivienda": 0,
+            "banos": 3,
+            "area_construida": 130,
+            "nom_comuna": 30,
+            "nom_ciudad": 5,
+            "nom_region": 0,
+            "nom_vecindario":111
+            }
 
-#response = requests.post(url, json=data, headers=headers)
-#res = round(response.json()['prediction'][0], 1)
-#print(res)
-#print(response.json())
+# Enviar la solicitud POST a la API
+response = requests.post(url, json=[vivienda], headers=headers)
 
-""" 
-Este código hace lo siguiente:
-
-Envía una solicitud POST a la API con los datos para los años futuros.
-Recibe las predicciones de la API.
-Itera sobre los años futuros y las predicciones, aplicando un incremento del 0.03 a cada predicción.
-Imprime el valor calculado por año con el incremento aplicado.
-
-"""
+# Verificar la respuesta
+if response.status_code == 200:
+    prediction = response.json()['prediction'][0]
+    print(f"Predicción del costo de la vivienda: {prediction} UF")
+else:
+    print(f"Error: {response.status_code}")
+    print(response.json())
 
 #otra predicción basada en años futuros
-future_years = [2024, 2025, 2026, 2027, 2028]
-data = []
-for year in future_years:
-    data.append({
-        "habitaciones": 3,
-        "pobreza": 10,
-        "area_total": 100,
-        "año": year
-    })
+#future_years = list(range(2022, 2027))
+#for year in future_years:
+#    data.append({
+#        "habitaciones": 3,
+#        "pobreza": 10,
+#        "area_total": 100,
+#        "año": year
+#    })
 
-response = requests.post(url, json=data, headers=headers)
-predictions = response.json()['prediction']
 
 # Imprimir el valor calculado por año con el incremento del 0.03
-incremented_predictions = []
-for i, (year, prediction) in enumerate(zip(future_years, predictions)):
-    incremented_value = prediction * (1 + 0.03) ** i
-    incremented_value = round(incremented_value, 1)
-    incremented_predictions.append(incremented_value)
-    print(f"Predicción para el año {year}: {incremented_value}")
+#incremented_predictions = []
+#for i, (year, prediction) in enumerate(zip(future_years, predictions)):
+#    incremented_value = prediction * (1 + 0.03) ** i
+#    incremented_value = round(incremented_value, 1)
+#    incremented_predictions.append(incremented_value)
+#    print(f"Predicción para el año {year}: {incremented_value}")
 
 # Graficar la proyección resultante
-plt.plot(future_years, incremented_predictions, marker='o')
-plt.xlabel('Año')
-plt.ylabel('Predicción de Precio (UF)')
-plt.title('Proyección de Precios de Vivienda con Incremento del 3% Anual')
-plt.grid(True)
-plt.show()
+#plt.plot(future_years, incremented_predictions, marker='o')
+#plt.xlabel('Año')
+#plt.ylabel('Predicción de Precio (UF)')
+#plt.title('Proyección de Precios de Vivienda con Incremento del 3% Anual')
+#plt.grid(True)
+#plt.show()
--- model.py
+++ model.py
@@ -5,28 +5,81 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 import datetime as dt
 #cargar el dataset en un dataframe
-url  = 'supabase_vivienda_table.csv'
+url  = 'supabase_Vivienda_Table.csv'
 data = pd.read_csv(url)
 
-data.fillna('', inplace=True)
-data['precio_uf'] = data['precio_uf'].astype(int)
-data['tipo_operacion'] = data['tipo_operacion'].astype(str)
-dic = {'True': 'Arriendo', 'False': 'Venta'}
-data['tipo_operacion']= data['tipo_operacion'].replace(dic)
-#data['id_vecindario'] = data['id_vecindario'].astype(int)
-data['tipo_vivienda'] = data['tipo_vivienda'].astype(int)
-data['habitaciones'] = data['habitaciones'].astype(int)
-data['fecha_creacion'] = pd.to_datetime(data['fecha_creacion'])
-#calcular el indice de pobreza
-data['pobreza'] = (data['precio_uf'] / data['area_total']) / data['habitaciones']
-data['año'] = data['fecha_creacion'].dt.year  # Asegúrate de tener una columna de fecha y extraer el año
-X = data[['habitaciones', 'pobreza', 'area_total', 'año']]  # variables independientes
-y = data['precio_uf']  # variable objetivo
+#Eliminamos columnas que no sirven para el modelo
+data.drop(['id_vivienda', 'nombre_propiedad', 'descripcion', 'ubicacion', 'fecha_creacion', 'links_contacto', 'id_vecindario',
+           'id_comuna', 'id_ciudad', 'id_region', 'tipo_subsidio', 'latitud', 'longitud'], axis=1, inplace=True)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+#eliminamos columnas que mantienen datos nulos
+data.dropna(axis=1, how='all', inplace=True)
 
+#Eliminamos columnas que contiene arriendo en True
+data.drop(data[data['tipo_operacion'] == True].index, inplace=True)
+
+#Eliminamos la columna de tipo de operacion
+data.drop('tipo_operacion', axis=1, inplace=True)
+
+#Cambiamos la columnas object a category
+for col in data.columns:
+    if data[col].dtype == 'object':
+        data[col] = data[col].astype('category')
+
+#imputacion de valores nulos
+for col in data.columns:
+    if pd.api.types.is_numeric_dtype(data[col]):
+        data[col] = data[col].fillna(data[col].mean())
+
+for col in data.columns:
+    if pd.api.types.is_categorical_dtype(data[col]):
+        data[col] = data[col].fillna(data[col].mode()[0])
+
+data.reset_index(drop=True, inplace=True)
+
+#tipo_vivienda categorica es necesario transformarla
+# Create a mapping dictionary
+tipo_vivienda_mapping = {0: 'Departamento', 1: 'Casa', 2: 'Otro'}
+
+# Apply the mapping to the 'tipo_vivienda' column
+data['tipo_vivienda'] = data['tipo_vivienda'].map(tipo_vivienda_mapping)
+data['tipo_vivienda'] = data['tipo_vivienda'].astype('category')
+
+#Confirmación de variables categoricas
+from sklearn.preprocessing import LabelEncoder
+encoders = {}
+categoricas = data.select_dtypes(include=['category', 'object']).columns
+
+for column in categoricas:
+    encoders[column] = LabelEncoder()  # Create a new LabelEncoder for each column.
+    data[column] = encoders[column].fit_transform(data[column])
+
+
+#tratamiento de los outliers
+outliers=['area_total',	'habitaciones',	'banos', 'area_construida', 'precio_uf']
+for col in outliers:
+  try:
+    max = data[col].describe()[6]+1.5*(data[col].describe()[6]-data[col].describe()[4]) #Q3+1.5*IQR
+  except:
+    continue
+  query = col+'>@max'
+  data.drop(data.query(query).index, inplace=True)
+
+data=data.reset_index(drop=True)
+
+
+#entrenamiento del modelo
+X = data.drop('precio_uf', axis=1)
+y = data['precio_uf']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=102)
+
+#regresión lineal
 model = LinearRegression()
 model.fit(X_train, y_train)
+# Imprimimos los coeficientes y el intercepto
+print(model.intercept_)
+print(model.coef_)
+
 #evaluar el modelo
 y_pred = model.predict(X_test)
 
@@ -34,14 +87,13 @@ y_pred = model.predict(X_test)
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 
+print(f"Error cuadrático medio (MSE): {mse}")
+print(f"R-cuadrado (R2): {r2}")
+
 #Guardar el modelo
 import joblib
 joblib.dump(model, 'modelo_vivienda.pkl')
 print('Modelo guardado')
-
-#cargar el modelo
-#model = joblib.load('modelo_vivienda.pkl')
-
 #guardar las columnas relevantes
 model_columns = list(X.columns)
 joblib.dump(model_columns, 'model_columns.pkl')
