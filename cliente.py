878a5ae029d91a1fffda96d77dcb703b82278caa
Magnus-Lekter <man.vargasc@duocuc.cl>
2024-11-25 01:17:37 -0300

    actualziación consulta cliente

--- cliente.py
+++ cliente.py
@@ -31,29 +31,43 @@ else:
     print(f"Error: {response.status_code}")
     print(response.json())
 
+data = []
 #otra predicción basada en años futuros
-#future_years = list(range(2022, 2027))
-#for year in future_years:
-#    data.append({
-#        "habitaciones": 3,
-#        "pobreza": 10,
-#        "area_total": 100,
-#        "año": year
-#    })
+future_years = list(range(2024, 2030))
+for year in future_years:
+   data.append({
+            "area_total": 139,
+            "habitaciones": 6,
+            "tipo_vivienda": 0,
+            "banos": 3,
+            "area_construida": 130,
+            "nom_comuna": 30,
+            "nom_ciudad": 5,
+            "nom_region": 0,
+            "nom_vecindario":111,
+            "año": year
+            })
 
+# Enviar la solicitud POST a la API
+response = requests.post(url, json=data, headers=headers)
 
-# Imprimir el valor calculado por año con el incremento del 0.03
-#incremented_predictions = []
-#for i, (year, prediction) in enumerate(zip(future_years, predictions)):
-#    incremented_value = prediction * (1 + 0.03) ** i
-#    incremented_value = round(incremented_value, 1)
-#    incremented_predictions.append(incremented_value)
-#    print(f"Predicción para el año {year}: {incremented_value}")
-
-# Graficar la proyección resultante
-#plt.plot(future_years, incremented_predictions, marker='o')
-#plt.xlabel('Año')
-#plt.ylabel('Predicción de Precio (UF)')
-#plt.title('Proyección de Precios de Vivienda con Incremento del 3% Anual')
-#plt.grid(True)
-#plt.show()
+# Verificar la respuesta
+if response.status_code == 200:
+    predictions = response.json()['prediction']
+    incremented_predictions = []
+    for i, prediction in enumerate(predictions):
+        incremented_value = prediction * (1 + 0.03) ** i
+        incremented_value = round(incremented_value, 1)
+        incremented_predictions.append(incremented_value)
+        print(f"Predicción para el año {future_years[i]}: {incremented_value} UF")
+    
+    # Graficar la proyección resultante
+    plt.plot(future_years, incremented_predictions, marker='o')
+    plt.xlabel('Año')
+    plt.ylabel('Predicción de Precio (UF)')
+    plt.title('Proyección de Precios de Vivienda con Incremento del 3% Anual')
+    plt.grid(True)
+    plt.show()
+else:
+    print(f"Error: {response.status_code}")
+    print(response.json())
